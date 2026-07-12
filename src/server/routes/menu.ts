3abab2d8e25ba018b185bf40de23d31bd44ed296
m-ahmad-amin <mahmadamindw@gmail.com>
2026-07-12 22:35:54 +0500

    Add menu file

--- src/server/routes/menu.ts
+++ src/server/routes/menu.ts
@@ -1,13 +1,13 @@
 import { Hono } from 'hono';
 import type { UiResponse } from '@devvit/web/shared';
 import { context } from '@devvit/web/server';
-import { createPost } from '../core/post';
+import { createDailyPost } from '../core/post';
 
 export const menu = new Hono();
 
 menu.post('/post-create', async (c) => {
   try {
-    const post = await createPost();
+    const post = await createDailyPost();
 
     return c.json<UiResponse>(
       {
